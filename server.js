6f33e08403000456aad22a14ba0d898e96d1c9d3
Donny <donthephan@gmail.com>
2022-01-10 19:09:00 +0000

    cookies working

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // load .env data into process.env
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+
 const database = require('./routes/database');
 
-// Temporary Dummy Login
-let user_id = 1;
 let { categories } = require('./seeds/constants');
 
 // Web server config
@@ -22,6 +23,17 @@ db.connect();
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [ 'superUltraSpecialSecretKey' ],
+    user_id: undefined
+  })
+);
+
 app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
@@ -54,6 +66,8 @@ app.use('/api/widgets', widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
+  let user_id = req.session.user_id;
+  console.log(user_id);
   database.getTasksFromUserId(user_id).then((tasks) => {
     // for (let category in categories) {
     //   $(`.${category}`).append(`
@@ -78,6 +92,10 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+  let { email } = req.body; // FOR ACTUAL login form
+
+  req.session.user_id = 2; // test different ids
+  console.log('cookie set!');
   res.redirect('/');
 });
 
