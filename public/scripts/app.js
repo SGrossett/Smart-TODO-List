fdf4e2acb1538efcf02b82ed517ec1ad5b7241c7
Safia Abdale <safia.abdale@gmail.com>
2022-01-11 15:40:51 +0000

    Updated styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,10 @@
-// JQuery
+// JQuery - add items to list
 $(document).ready(function() {
   console.log('jquery loaded');
+
   $('#form1').submit(function(event) {
     const taskText = $('#task-text').val();
+    const markup = `<p class="listItem">${taskText}<button class="deleteButton">X</button></p>`;
     event.preventDefault();
     console.log('submitting form')
 
@@ -10,9 +12,25 @@ $(document).ready(function() {
       url: `/tasks`,
       method: 'POST',
       data: $(this).serialize()
-    }).done(tweetArr => {
-      $('.container').append(`<div>${taskText}</div>`);
+    }).done(listItems => {
+      // add filter here
+      $('.movieTasks').append(markup);
       console.log('appending the element');
     })
   })
-})
+});
+// JQuery - accordion list items
+$( function() {
+  $( "#accordion" ).accordion({
+    collapsible: true
+  });
+  $('#accordion').accordion("refresh")
+});
+
+
+// delete button
+// $(".deleteButton").click(function() {
+//   $(".movieTasks").eq(1).remove();
+// });
+// console.log(`delete button being pushed`);
+// add filter here
