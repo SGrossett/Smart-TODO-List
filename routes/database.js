1f43a54ca6ea2a1dbcb63f280322d19fcbc7b3fe
Selene Grossett <selene_816@hotmail.com>
2022-01-11 04:05:02 +0000

    Add code to fiter catergories for task.

--- routes/database.js
+++ routes/database.js
@@ -72,15 +72,56 @@ const addUserWithEmail = function(email) {
 };
 
 const insertIntoTasks = function (text) {
+  const task = text;
+  console.log('task:', task)
+  const allKeyWords = [
+    'movie',      'watch',       'show',
+    'drama',      'Netflix',     'Hulu',
+    'Disney',     'HBO',         'cartoon',
+    'anime',      'Crunchyroll', 'cable',
+    'restaurant', 'dinner',      'eat',
+    'takeout',    'food',        'menu',
+    'dine',       'lunch',       'brunch',
+    'drink',      'tea',         'bar',
+    'cafe',       'book',        'read',
+    'library',    'more',        'buy'
+    ];
+
+
+  let keyWord = '';
+  for (let word of allKeyWords) {
+    if (task.toLowerCase().includes(word.toLowerCase())) {
+      keyWord = word;
+      break;
+    } else {
+      keyWord = '';
+    }
+  }
+
+  const categoryKeys = {
+    movie: ['movie', 'watch', 'show', 'drama', 'Netflix', 'Hulu', 'Disney', 'HBO', 'cartoon', 'anime', 'Crunchyroll', 'cable'],
+    restaurant: ['restaurant', 'dinner', 'eat', 'takeout', 'food', 'menu', 'dine', 'lunch', 'brunch', 'drink', 'tea', 'bar', 'cafe'],
+    book: ['book', 'read', 'library'],
+    product: ['more', 'buy']
+  }
+
+  let category = '';
+  if (categoryKeys['movie'].includes(keyWord)) category = 'movie';
+  if (categoryKeys['restaurant'].includes(keyWord)) category = 'restaurant';
+  if (categoryKeys['book'].includes(keyWord)) category = 'book';
+  if (categoryKeys['product'].includes(keyWord)) category = 'product';
+
   return pool.query(`
     INSERT INTO tasks (
       description,
+      category,
       date_created
     ) VALUES (
       $1,
+      $2,
       NOW()
     )
-  `, [text])
+  `, [text, category])
   .then((result) => result)
   .catch((err) => console.log('Error:', err.message) );
 }
