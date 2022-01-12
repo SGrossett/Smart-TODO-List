b13fb9bed5fcbded3b08169a84c9f040e7b298bf
Donny <donthephan@gmail.com>
2022-01-12 21:56:37 +0000

    migrate category assigner to categoryAssigner function

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const categoryAssigner = require('./tools/categoryAssigner');
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
@@ -20,93 +21,7 @@ module.exports = (db) => {
     const text = req.body.text;
     const user_id = req.cookies.user_id;
 
-    console.log('task:', text, ', user_id:', user_id);
-    const allKeyWords = [
-      'movie',
-      'watch',
-      'show',
-      'drama',
-      'Netflix',
-      'Hulu',
-      'Disney',
-      'HBO',
-      'cartoon',
-      'anime',
-      'Crunchyroll',
-      'cable',
-      'restaurant',
-      'dinner',
-      'eat',
-      'takeout',
-      'food',
-      'menu',
-      'dine',
-      'lunch',
-      'brunch',
-      'drink',
-      'tea',
-      'bar',
-      'cafe',
-      'book',
-      'read',
-      'library',
-      'more',
-      'buy'
-    ];
-
-    let keyWord = '';
-    for (let word of allKeyWords) {
-      if (text.toLowerCase().includes(word.toLowerCase())) {
-        keyWord = word;
-        break;
-      } else {
-        keyWord = '';
-      }
-    }
-
-    const categoryKeys = {
-      movie: [
-        'movie',
-        'watch',
-        'show',
-        'drama',
-        'Netflix',
-        'Hulu',
-        'Disney',
-        'HBO',
-        'cartoon',
-        'anime',
-        'Crunchyroll',
-        'cable'
-      ],
-      restaurant: [
-        'restaurant',
-        'dinner',
-        'eat',
-        'takeout',
-        'food',
-        'menu',
-        'dine',
-        'lunch',
-        'brunch',
-        'drink',
-        'tea',
-        'bar',
-        'cafe'
-      ],
-      book: [ 'book', 'read', 'library' ],
-      product: [ 'more', 'buy' ]
-    };
-
-    let category = '';
-    if (categoryKeys['movie'].includes(keyWord)) category = 'movie';
-    if (categoryKeys['restaurant'].includes(keyWord)) category = 'restaurant';
-    if (categoryKeys['book'].includes(keyWord)) category = 'book';
-    if (categoryKeys['product'].includes(keyWord)) category = 'product';
-
-    if (!category) {
-      category = 'product';
-    }
+    const category = categoryAssigner(text);
 
     db
       .query(
