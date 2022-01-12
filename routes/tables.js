b9235a8111c63bdf8569b4078cc78dc3cb3c9ee5
Safia Abdale <safia.abdale@gmail.com>
2022-01-12 03:33:35 +0000

    Created routes folder

---
+++ routes/tables.js
@@ -0,0 +1,18 @@
+/*
+ * All routes for Tables are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/user-tasks", (req, res) => {
+    let query
+  })
+}
+
+
+// add routes from server.js
