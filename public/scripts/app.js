03d5cf655cb626c5b27dfd307869b72465fb79ec
Safia Abdale <safia.abdale@gmail.com>
2022-01-13 01:25:36 +0000

    Added function to remove item from list on button-click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   console.log('jquery loaded');
   let user_id;
 
+  /* ------Adding Items--------------*/
   $('#form1').submit(function(event) {
     event.preventDefault();
-    console.log('submitting form');
 
     $.ajax({
       url: `/user-tasks`,
@@ -13,7 +13,6 @@ $(document).ready(function() {
       data: $(this).serialize()
     })
       .done((listItems) => {
-        console.log('TASK ADDED!');
         fetchTasks();
       })
       .fail((err) => {
@@ -21,8 +20,9 @@ $(document).ready(function() {
       });
   });
 
-  const createMarkup = function(data) {
-    return `<div><span class="listItem"><input type="checkbox" id="list-delete"></input>${data.description}</span></div>`;
+
+  const createMarkup = function(item, selectorID) {
+    return `<div><span class="listItem"><button id="${selectorID}"></button>${item.description}</span></div>`;
   };
 
   const fetchTasks = function() {
@@ -33,38 +33,72 @@ $(document).ready(function() {
       $('.listContents').empty();
 
       listItems.map((item) => {
+        const selectorID = `list-item-${item.id}`
         // add item to movies
         if (item.category === 'movie') {
-          $('.movieTasks').prepend(createMarkup(item));
+          $('.movieTasks').prepend(createMarkup(item, selectorID));
         }
         // add item to restaurants
         if (item.category === 'restaurant') {
-          $('.restaurantTasks').prepend(createMarkup(item));
+          $('.restaurantTasks').prepend(createMarkup(item, selectorID));
         }
         // add item to books
         if (item.category === 'book') {
-          $('.bookTasks').prepend(createMarkup(item));
+          $('.bookTasks').prepend(createMarkup(item, selectorID));
         }
         // add item to products
         if (item.category === 'product') {
-          $('.productTasks').prepend(createMarkup(item));
+          $('.productTasks').prepend(createMarkup(item, selectorID));
         }
+        $(`#${selectorID}`).click(function(event) {
+          event.preventDefault();
+          console.log('submitting form');
+          finishTask(item.id);
+        });
       });
     });
   };
 
+
+
+  /* ------------Accordion-----------*/
   $('.accordion').accordion({
     collapsible: true,
     active: false
   });
   $('.accordion').accordion('refresh');
 
+  /* ------User verification--------*/
+
   $.get('/cookie_user_id', (res) => {
     user_id = res;
     fetchTasks();
   });
+
+  /* --------Removing Items----------*/
+  /*
+    add date_finished, same as date_created
+    add filter to list, to remove from main page
+  */
+
+
+  const finishTask = function(id) {
+    $.ajax({
+      url: `/user-tasks/delete`,
+      method: 'POST',
+      data: { id }
+    })
+      .done((listItems) => {
+        console.log('TASK REMOVED!');
+        fetchTasks();
+      })
+      .fail((err) => {
+        console.log('ERROR', err);
+      });
+  }
 });
 
+// REMOVE - this is for the old header
 // used in _header partial
 function navbarToggle() {
   var x = document.getElementById('menu-links');
