909b0b757c6bfe207ecee321ea9c9adf839136cb
Selene Grossett <selene_816@hotmail.com>
2022-01-13 08:04:45 +0000

    Add event listens so task and hamburger work together correctly.

--- public/scripts/finishedClient.js
+++ public/scripts/finishedClient.js
@@ -1,51 +1,5 @@
 $(document).ready(function() {
 
-  // const createTaskMarkup = function(taskObj) {
-  //   const $task = $(
-  //     `<label class="form-control">
-  //       <input type="checkbox" class="checkbox incomplete" id="${taskObj.id}" name="${taskObj.category}" value="${taskObj.description.replace(/ /g, '-')}">
-  //       ${taskObj.description}
-  //     </label>`
-  //   );
-  //   return $task;
-  // }
-
-  // const renderTasks = function(tasks) {
-  //   for (let task of tasks) {
-  //     const $task = createTaskMarkup(task);
-  //     $(`#task-container`).prepend(task)
-  //   }
-  // }
-
-  // const fetchTasks = function(){
-  //   $.ajax({
-  //     url: `/completed`,
-  //     method: 'GET',
-  //     success: (tasks) => {
-  //       $('.form-control').empty()
-  //       renderTasks(tasks)
-  //     },
-  //     error: (err) => console.log(`Error: ${err}`)
-  //   });
-  // };
-
-  // fetchTasks();
-
-  // $.ajax({
-
-  // })
-
-  $.ajax({
-    url: `/completed`,
-    method: 'GET',
-    success: (tasks) => {
-
-      console.log(tasks)
-    },
-    error: (err) => console.log(`Error: ${err}`)
-  });
-
-
   $('input[type="checkbox"]').on('click', function(){
     let id = $(this).attr('id');
     if($(this).is(":checked")){
@@ -61,6 +15,9 @@ $(document).ready(function() {
         })
         console.log(`${id} is unchecked`);
     }
+  });
 
+  $(`#burger`).on('click', function() {
+    $(`.container`).toggle(600);
   });
 });
